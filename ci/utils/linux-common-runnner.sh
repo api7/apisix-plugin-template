3bdd3c4d602d5f19846466440bc89e7f56cfe3b2
leslie <leslie.tsang@icloud.com>
2022-01-24 03:59:55 +0800

    feat: add basic CI utils script support

---
+++ ci/utils/linux-common-runnner.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+VAR_CUR_PATH="$(cd $(dirname ${0}); pwd)"
+
+source "${VAR_CUR_PATH}/linux-common.sh"
+
+# =======================================
+# Linux common config
+# =======================================
+get_apisix_code() {
+    # ${1} branch name
+    git_branch=${1:release/2.10}
+    git clone --recursive https://github.com/apache/apisix.git -b "${git_branch}" && cd apisix || exit 1
+}
+
+# =======================================
+# Entry
+# =======================================
+case_opt=$1
+shift
+
+case ${case_opt} in
+get_apisix_code)
+    get_apisix_code "$@"
+    ;;
+*)
+    func_echo_error_status "Unknown method: ${case_opt}"
+    ;;
+esac
