6b0451f5630b3a05d13ac359a886ee9f340aea9d
leslie <leslie.tsang@icloud.com>
2022-01-24 09:18:16 +0800

    chore: change git clone behavior

--- ci/utils/linux-common-runnner.sh
+++ ci/utils/linux-common-runnner.sh
@@ -12,7 +12,7 @@ source "${VAR_CUR_PATH}/linux-common.sh"
 get_apisix_code() {
     # ${1} branch name
     git_branch=${1:-release/2.10}
-    git clone --recursive https://github.com/apache/apisix.git -b "${git_branch}" && cd apisix || exit 1
+    git clone --depth 1 --recursive https://github.com/apache/apisix.git -b "${git_branch}" && cd apisix || exit 1
 }
 
 # =======================================
